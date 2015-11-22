a2883d4698ce25f53aa8fb36ef60c1f7381209d3
unknown <renan.lifestyle@hotmail.com>
2015-11-22 21:03:43 -0200

    like button v1.0 finished

--- jsx/LikeButton.jsx.js
+++ jsx/LikeButton.jsx.js
@@ -1,4 +1,5 @@
 var React = require('react')
+var CssFileLoader = require('../lib/CssFileLoader.js')
 
 module.exports = React.createClass({
   displayName: 'LikeButton.jsx',
@@ -7,16 +8,33 @@ module.exports = React.createClass({
       liked: false
     }
   },
+  componentDidMount: function () {
+    CssFileLoader.load('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css')
+  },
   getLabel: function () {
-    return this.state.liked ? 'liked' : 'like'
+    return this.isLiked() ? 'liked' : 'like'
+  },
+  getIcon: function () {
+    return this.isLiked() ? 'fa-heart' : 'fa-heart-o'
   },
   like: function () {
-
+    if (!this.isLiked()) {
+      this.setState({
+        liked: true
+      })
+    }
+  },
+  isLiked: function () {
+    return this.state.liked
   },
   render: function () {
     const label = this.getLabel()
+    const disabled = this.isLiked()
+    const icon = this.getIcon()
+
     return (
-      <button className='LikeButton' onClick={this.like} ref='button'>
+      <button className='LikeButton' onClick={this.like} ref='button' disabled={disabled}>
+        <i className={'fa ' + icon}></i>
         {label}
       </button>
     )
--- test/unit.js
+++ test/unit.js
@@ -11,11 +11,60 @@ test('Like Button Constructor', (t) => {
 
   var likeButton = TestUtils.renderIntoDocument(
     <LikeButton />
-  );
+  )
 
-  var likeButtonNode = ReactDOM.findDOMNode(likeButton);
+  var likeButtonNode = ReactDOM.findDOMNode(likeButton)
 
   t.equal(likeButtonNode.textContent, 'like')
+
+  t.end()
+})
+
+test('Like Button should be disable once clicked', (t) => {
+  t.plan(1)
+
+  var likeButton = TestUtils.renderIntoDocument(
+    <LikeButton />
+  )
+
+  var likeButtonNode = ReactDOM.findDOMNode(likeButton)
+  TestUtils.Simulate.click(likeButtonNode)
+
+  t.ok(likeButtonNode.disabled)
+
+  t.end()
+})
+
+test('Like Button label should be "liked"', (t) => {
+  t.plan(1)
+
+  var likeButton = TestUtils.renderIntoDocument(
+    <LikeButton />
+  )
+
+  var likeButtonNode = ReactDOM.findDOMNode(likeButton)
+  TestUtils.Simulate.click(likeButtonNode)
+
+  t.equal(likeButtonNode.textContent, 'liked')
+
+  t.end()
+})
+
+test('Like Button should change their state once', (t) => {
+  t.plan(2)
+
+  var likeButton = TestUtils.renderIntoDocument(
+    <LikeButton />
+  )
+
+  var likeButtonNode = ReactDOM.findDOMNode(likeButton)
+  TestUtils.Simulate.click(likeButtonNode)
+
+  t.equal(likeButtonNode.textContent, 'liked')
+
+  TestUtils.Simulate.click(likeButtonNode)
+
+  t.equal(likeButtonNode.textContent, 'liked')
 
   t.end()
 })
